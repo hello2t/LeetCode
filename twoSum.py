a382b397a22235d609093b80dcbb6909aa02b6d4
hello2t <hello2t@qq.com>
2015-09-15 22:26:55 +0800

    add twoSum

---
+++ twoSum.py
@@ -0,0 +1,30 @@
+def twoSum(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[nt]
+    """
+    sorted_num = sorted(nums)
+
+    # two points
+    left = 0
+    right = len(nums) - 1
+    while (left < right):
+        sum = sorted_num[left] + sorted_num[right]
+        if sum == target:
+            # find out index
+            break
+        elif sum > target:
+            right -= 1
+        else:
+            left += 1
+
+    if left == right:
+        return -1, -1
+    else:
+        pos1 = nums.index(sorted_num[left]) + 1
+        pos2 = nums.index(sorted_num[right]) + 1
+        if pos1 == pos2:    # find again
+            pos2 = nums[pos1:].index(sorted_num[right]) + pos1 + 1
+
+        return min(pos1, pos2), max(pos1, pos2)
